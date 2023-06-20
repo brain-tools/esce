7e1ca8c94ce426ad4c331b25f22c63d3858e5934
Laura <lauraflyra@gmail.com>
2023-06-20 17:19:37 +0200

    add parameter path to python file

--- workflow/scripts/generate_splits.py
+++ workflow/scripts/generate_splits.py
@@ -215,23 +215,24 @@ def write_splitfile(
         json.dump(split_dict, f, cls=NpEncoder, indent=0)
 
 
-n_train = int(snakemake.wildcards.samplesize)
-n_val = n_test = min(
-    round(n_train * snakemake.params.val_test_frac), snakemake.params.val_test_max
-) if snakemake.params.val_test_max else round(n_train * snakemake.params.val_test_frac)
-n_val = n_test = max(n_val, snakemake.params.val_test_min) if snakemake.params.val_test_min else n_val
-assert n_train > 1 and n_val > 1 and n_test > 1
+if __name__ == "__main__":
+    n_train = int(snakemakeSimplemm.wildcards.samplesize)
+    n_val = n_test = min(
+        round(n_train * snakemake.params.val_test_frac), snakemake.params.val_test_max
+    ) if snakemake.params.val_test_max else round(n_train * snakemake.params.val_test_frac)
+    n_val = n_test = max(n_val, snakemake.params.val_test_min) if snakemake.params.val_test_min else n_val
+    assert n_train > 1 and n_val > 1 and n_test > 1
 
 
-write_splitfile(
-    features_path=snakemake.input.features,
-    targets_path=snakemake.input.targets,
-    split_path=snakemake.output.split,
-    sampling_path=snakemake.input.matching,
-    sampling_type=snakemake.wildcards.matching,
-    n_train=n_train,
-    n_val=n_val,
-    n_test=n_test,
-    seed=int(snakemake.wildcards.seed),
-    stratify=snakemake.params.stratify,
-)
+    write_splitfile(
+        features_path=snakemake.input.features,
+        targets_path=snakemake.input.targets,
+        split_path=snakemake.output.split,
+        sampling_path=snakemake.input.matching,
+        sampling_type=snakemake.wildcards.matching,
+        n_train=n_train,
+        n_val=n_val,
+        n_test=n_test,
+        seed=int(snakemake.wildcards.seed),
+        stratify=snakemake.params.stratify,
+    )
