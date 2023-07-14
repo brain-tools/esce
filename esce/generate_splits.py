387034232215878eb727f9a67add57f675aa1c77
Chung-Fan <airolg1111@gmail.com>
2023-07-14 11:46:07 +0200

    fix new test bugs with typing

--- esce/generate_splits.py
+++ esce/generate_splits.py
@@ -6,7 +6,7 @@ import numpy as np
 from imblearn.under_sampling import RandomUnderSampler
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-
+from typing import Tuple
 
 class NpEncoder(json.JSONEncoder):
     """Encode numpy arrays to JSON."""
@@ -29,7 +29,7 @@ def generate_random_split(
     do_stratify: bool = False,
     seed: int = 0,
     mask: Optional[np.ndarray] = False,
-) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """Generate a random split of the data."""
     if mask is False:
         idx_originial = np.arange(len(y))
@@ -74,7 +74,7 @@ def generate_matched_split(
     do_stratify: bool = False,
     seed: int = 0,
     mask: Optional[np.ndarray] = False,
-) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """Generate a matched split of the data.
 
     Assumes a binary classification target variable, coded as 0 and 1, with 1 being the positive (patient) class and 0 being the negative (control) class.
